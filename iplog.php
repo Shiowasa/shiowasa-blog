2d03820dfda73900d7278ce6e003d1c183b50279
shio <mytr2dreams@gmail.com>
2026-03-07 12:52:38 +0900

    update　blog
    練習用ブログの使用運転

---
+++ iplog.php
@@ -0,0 +1,12 @@
+<?php
+// アクセス日時とIPを取得
+$ip = $_SERVER['REMOTE_ADDR'];
+$date = date("Y-m-d H:i:s");
+
+// iplog.txt に追記
+file_put_contents("iplog.txt", "$date - $ip\n", FILE_APPEND);
+
+// index.html に戻す
+header("Location: index.html");
+exit;
+?>
